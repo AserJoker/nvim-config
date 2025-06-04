0edaeda4a18546461693bd06786395165f4e50d2
AserJoker <AserJoker@outlook.com>
2025-06-04 16:17:19 +0800

    Update mason.lua

--- lua/config/mason.lua
+++ lua/config/mason.lua
@@ -2,7 +2,6 @@ local function setup()
   require 'mason'.setup({
     log_level = vim.log.levels.DEBUG,
     pip = {
-      install_args = { '--proxy', 'http://w30029682:19980108wjf+-@proxy.huawei.com:8080/' }
     }
   })
 end
